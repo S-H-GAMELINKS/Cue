134946b47839c1af160d48d31a55a63e4039a7c0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-13 19:06:29 +0900

    add cue/assets/webpack.config.js

---
+++ cue/assets/webpack.config.js
@@ -0,0 +1,31 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: { 
+    filename: 'index.js',
+    path: `${__dirname}` 
+  },
+  module: {
+    rules: [
+    {
+      test: /\.vue$/,
+      use: 'vue-loader'
+    },
+    {
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            'css-loader'
+        ]
+    }]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
