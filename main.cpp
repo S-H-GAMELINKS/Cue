11a4bf7e7ab628d8bca328dc9e2b7901d6c37bb2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-13 19:17:03 +0900

    create dir & copy template

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 int main(int argc, char* argv[]) {
+
+    fs::create_directories(argv[1]);
+
+    fs::copy("cue", argv[1]);
 
     return 0;
 }
