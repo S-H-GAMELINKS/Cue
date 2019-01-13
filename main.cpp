fc691cd239212a3a154c11e0726a5878cdefaf42
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-13 20:04:01 +0900

    add cue template for new command

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <filesystem>
+#include <iostream>
+#include <string>
 
 namespace fs = std::filesystem;
 
+void CreateCue(const char* argv) {
+    fs::path path = argv;
+    fs::path cue = "./cue/cue";
+
+    fs::copy(cue, path, fs::copy_options::recursive);
+
+    std::cout << "Create Cue Template!" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
 
-    fs::create_directories(argv[1]);
+    std::string cmd = argv[1];
 
-    fs::copy("cue", argv[1]);
+    if (cmd == "new") {
+        CreateCue(argv[2]);
+    }
 
     return 0;
 }
