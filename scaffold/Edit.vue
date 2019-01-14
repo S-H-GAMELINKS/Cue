6f061a1639ccacd32e039814c72be2ec60a9b1ac
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-14 23:37:43 +0900

    add scaffold/Edit.vue

---
+++ scaffold/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <scaffold-form></scaffold-form>
+    </div>
+</template>
+
+<script>
+import Form from './Form.vue';
+
+export default {
+    components: {
+        'scaffold-form': Form
+    },
+}
+</script>
