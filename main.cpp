dd2eef27d51ab618c2e40643815e618ce57bf188
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-14 21:52:15 +0900

    add scaffold command

--- main.cpp
+++ main.cpp
@@ -13,12 +13,23 @@ void CreateCue(const char* argv) {
     std::cout << "Create Cue Template!" << std::endl;
 }
 
+void ScaffoldCue(const char* argv) {
+    fs::path path = "assets/components/" + argv;
+    fs::path scaffold = "./cue/scaffold";
+
+    fs::copy(scaffold, path, fs::copy_options::recursive);
+
+    std::cout << "Scaffold " << argv << " Template!" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
 
     std::string cmd = argv[1];
 
     if (cmd == "new") {
         CreateCue(argv[2]);
+    } else if (cmd == "scaffold") {
+        ScaffoldCue(argv[2]);
     }
 
     return 0;
